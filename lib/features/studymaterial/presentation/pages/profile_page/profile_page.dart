a1aee4900145c45aa0a6ac30e674c8040b965060
SK1PPR <agrawalkhushal04@gmail.com>
2025-01-03 16:05:51 +0530

    Adds activity page

--- lib/features/studymaterial/presentation/pages/profile_page/profile_page.dart
+++ lib/features/studymaterial/presentation/pages/profile_page/profile_page.dart
@@ -1,12 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:studyportal/features/studymaterial/presentation/pages/home_page/components/more_info_button.dart';
+import 'package:studyportal/features/studymaterial/presentation/pages/activity_page/activity_page.dart';
 
 class ProfilePage extends StatelessWidget {
   const ProfilePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(child: Text("Profile Page")),
+    return Scaffold(
+      body: Container(
+        child: MoreInfoButton(
+            onTap: () {
+              Navigator.of(context).push(MaterialPageRoute(
+                  builder: (context) => const ActivityPage()));
+            },
+            info: "Activity",
+            icon: SvgPicture.asset(
+              "lib/core/svgs/right_caret.svg",
+              height: 16,
+              width: 16,
+            )),
+      ),
     );
   }
 }
